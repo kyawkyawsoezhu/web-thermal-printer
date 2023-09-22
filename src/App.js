97aabc245cb139d696b6983c078e785abd184095
Kyaw Kyaw Soe <kyawkyawsoe.zhu@gmail.com>
2023-09-22 17:53:07 +0700

    add build files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Br, Cut, Line, Printer, Row, Text, render } from "react-thermal-printer";
 
 function App() {
+  const receipt = (
+    <Printer type="epson" width={42} characterSet="korea" debug={true}>
+      <Text size={{ width: 2, height: 2 }}>9,500원</Text>
+      <Text bold={true}>결제 완료</Text>
+      <Br />
+      <Row
+        gap={1}
+        left={<Text size={{ width: 2, height: 2 }}>포</Text>}
+        center={<Text size={{ width: 2, height: 2 }}>알로하 포케 맛있는 거</Text>}
+        right="X 15"
+      />
+      <Line />
+      <Cut />
+    </Printer>
+  );
+
+  const [port, setPort] = useState();
+
+  const print = async () => {
+    let _port = port;
+    console.log(_port);
+    if (!_port) {
+      console.log('_port');
+      _port = await navigator.serial.requestPort();
+      await _port.open({ baudRate: 115200 });
+      setPort(_port);
+    }
+
+    const writer = _port.writable?.getWriter();
+    if (!writer) {
+      const data = await render(receipt);
+      await writer.write(data);
+      writer.releaseLock();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{receipt}</div>
+      <hr style={{ margin: "20px 0" }} />
+      <button onClick={print}>
+        Print
+      </button>
     </div>
   );
 }
